1233114d7a7a4a6e435fac37a344e94ad686da6d
gohkh <gohkh2011@gmail.com>
2020-05-19 21:06:42 +0800

    Add the ability to update orders in OrderHistory

--- src/main/java/gohkh/serveup/model/OrderHistory.java
+++ src/main/java/gohkh/serveup/model/OrderHistory.java
@@ -19,4 +19,15 @@ public class OrderHistory {
         return new OrderHistory(Stream.concat(orders.stream(), Stream.of(toAdd))
                 .collect(Collectors.toUnmodifiableList()));
     }
+
+    public OrderHistory update(Order toUpdate) {
+        Function<Order, Order> updateOrder = order -> {
+            if (toUpdate.equals(order)) {
+                return toUpdate;
+            }
+            return order;
+        };
+        return new OrderHistory(orders.stream().map(updateOrder)
+                .collect(Collectors.toUnmodifiableList()));
+    }
 }
