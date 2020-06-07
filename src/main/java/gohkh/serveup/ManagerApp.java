b8f1d77eff96566d17584d25ea3138a6394f3762
gohkh <gohkh2011@gmail.com>
2020-06-07 23:35:19 +0800

    Create the Manager application

---
+++ src/main/java/gohkh/serveup/ManagerApp.java
@@ -0,0 +1,25 @@
+package gohkh.serveup;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import gohkh.serveup.ui.Ui;
+import gohkh.serveup.ui.manager.ManagerUi;
+
+public class ManagerApp extends Application {
+    private Ui ui;
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void init() {
+        ui = new ManagerUi();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        ui.start(stage);
+    }
+}
